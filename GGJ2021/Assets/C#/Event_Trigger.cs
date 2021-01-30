1836acf457e49d286eed8ff6f04cf3f366faf757
CluClu <hugocluzel17@hotmail.fr>
2021-01-30 01:21:51 +0100

    Debut 3C
    c pas ouf mais tkt

---
+++ GGJ2021/Assets/C#/Event_Trigger.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Event_Trigger : MonoBehaviour
+{
+    //CanvasManager cm;
+    CameraManager camM;
+    [SerializeField] string nameEvent;
+    private bool done = false;
+
+    [Header("Dialogues")]
+     public string[] dialogues;
+     public AudioClip audioC;
+
+    private void Awake()
+    {
+        camM = CameraManager.Instance;
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !done)
+        {
+            EventAudio();
+            EventDialogue();
+
+            done = true;
+        }
+    }
+
+    private void EventAudio()
+    {
+        if (audioC)
+        {
+            //Debug.Log(camM + " Manager Cam  " + audioC);
+            camM.LaunchSound(audioC);
+        }
+    }
+
+    private void EventDialogue()
+    {
+        /*
+        if (cm.dialogueHere && dialogues.Length > 0)
+        {
+            cm.StartDiaEffect(dialogues);
+        }*/
+    }
+
+    #region EditMoi
+    private void OnDrawGizmos()
+    {
+        gameObject.name = "Evt " + nameEvent;
+    }
+
+    #endregion
+}
---
+++ GGJ2021/Assets/C#/Player_movement.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    public bool InCinematic;
+
+    [Header("Components")]
+    private CapsuleCollider2D cc;
+    private Rigidbody2D rb;
+
+    [Header("Movement")]
+    [SerializeField][Range(0, 200)] float speed;
+    [SerializeField] Vector2 forceJump;
+    [SerializeField] SpriteRenderer spritePlayer;
+
+    [Header("OnGround")]
+    [SerializeField] bool onGround;
+    [SerializeField] LayerMask layerGround;
+
+    private void Awake()
+    {
+        cc = GetComponent<CapsuleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (!InCinematic)
+        {
+            if (onGround)
+            {
+                Movement();
+            }
+        }
+    }
+    private void Update()
+    {
+        onGround = CheckGround();
+        if (!InCinematic)
+        {
+            Jump();
+        }
+    }
+
+    private void Movement()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed, 0);
+
+            spritePlayer.transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal"), 1, 1);
+        }
+        //print(rb.velocity.x);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && onGround)
+        {
+            rb.AddForce(forceJump);
+        }
+    }
+
+    private bool CheckGround()
+    {
+        float extraHeight = 0.05f;
+        //RaycastHit2D hit = Physics2D.Raycast(cc.bounds.center, Vector2.down, cc.bounds.extents.y + extraHeight);
+        RaycastHit2D hit = Physics2D.CircleCast(cc.bounds.center - new Vector3(0,0.65f,0), 0.4f, Vector2.down, 0.01f, layerGround);
+
+        Debug.DrawRay(cc.bounds.center, Vector2.down * (cc.bounds.extents.y + extraHeight));
+
+        if (hit.collider != null)
+        {
+            // && hit.collider.gameObject.layer == layerGround
+            print("bouh" + hit.collider.name);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
