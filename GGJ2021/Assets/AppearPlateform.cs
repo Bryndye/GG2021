5aead348c13599959f76ecba367d30d4dde48a20
CluClu <hugocluzel17@hotmail.fr>
2021-01-30 01:50:42 +0100

    SAVE MOI

---
+++ GGJ2021/Assets/AppearPlateform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppearPlateform : MonoBehaviour
+{
+    private void Update()
+    {
+        CheckLayer();
+    }
+
+    private void CheckLayer()
+    {
+
+    }
+}
