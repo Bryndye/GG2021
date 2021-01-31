6c4a36f652d330680c2324b2533e18a289a7dee0
CluClu <hugocluzel17@hotmail.fr>
2021-01-31 11:12:03 +0100

    Endijg cv + menu

---
+++ GGJ2021/Assets/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level Build");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
